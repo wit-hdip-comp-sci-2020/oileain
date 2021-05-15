208875c58a2affb87e08f5a5be591b97b3c66c71
Eamonn de Leastar <edeleastar@wit.ie>
2021-05-15 08:55:03 +0100

    create oileain service object and set in context

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,10 @@
   import Header from "./components/Header.svelte";
   import NotFound from "./pages/NotFound.svelte";
   import Home from "./pages/Home.svelte";
+  import {setContext} from "svelte";
+  import {Oileain} from "./services/oileain-api";
+
+  setContext("oileain", new Oileain());
 
   let routes = {
     "/": Home,
