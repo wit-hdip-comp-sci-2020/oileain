73b7f7af6425d3c05c1e4d884e3b355f0ac33c87
Eamonn de Leastar <edeleastar@wit.ie>
2021-05-19 06:54:14 +0100

    component to display lat/lng

---
+++ src/components/IslandLatLng.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { Island } from "../services/oileain-types";
+  export let island: Island;
+</script>
+
+<div class="uk-card uk-card-default uk-card-body uk-padding-small">
+  <caption>GPS-compatible</caption>
+  <table class="uk-table uk-table-divider uk-table-small">
+    <tbody>
+      <tr>
+        <td>Latitude</td>
+        <td>{island.coordinates.geo.lat}</td>
+      </tr>
+      <tr>
+        <td>Longitude</td>
+        <td>{island.coordinates.geo.long}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
